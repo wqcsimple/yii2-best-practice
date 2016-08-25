19aacf73f14989377d7ec3e897be7ffc03905895
whis <wqcsimple@foxmail.com>
2016-08-25 17:15:42 +0800

    update

--- config/params_extra.php
+++ config/params_extra.php
@@ -9,11 +9,11 @@ return [
 
     'redis-param' => [
         'scheme' => 'tcp',
-        'host'   => 'redis',
+        'host'   => 'redis.duapp.com',
         'port'   => 6379,
-        'password' => null,
+        'password' => '61438c2c4dfab85640fcc0b738f05c49Hide',
         'read_write_timeout' => 0,
-        'database' => 0,
+        'database' => 'hTxMSSKPqJTVYZoUJGLB',
     ],
 
 ];
